871dd1edf0cc1fed95ffd71ce829016f7dd467cc
Michel Vuijlsteke <michel@zog.org>
2015-07-18 01:34:37 +0200

    eerste ruwe versie

--- repertorium/beheer/admin.py
+++ repertorium/beheer/admin.py
@@ -1,5 +1,21 @@
 from django.contrib import admin
 
-from .models import Persoon
+from .models import Persoon, Rhetorica, Adres
 
-admin.site.register(Persoon)
+class AdresInline(admin.TabularInline):
+	model = Adres
+	extra = 0
+
+class PersoonAdmin(admin.ModelAdmin):
+	fieldsets = [
+		(None,         {'fields': ['voornaam', 'achternaam', 'rhetorica', 'email', 'oudid', 'wijziging']}),
+		('Personalia', {'fields': ['geboortedatum', 'sterfdatum'], 'classes': ['collapse']}),
+	]
+	inlines = [AdresInline]
+	list_display = ('voornaam', 'achternaam', 'rhetorica', 'wijziging')
+	list_filter = ['wijziging']
+	search_fields = ['voornaam', 'achternaam']
+	
+admin.site.register(Persoon, PersoonAdmin)
+
+admin.site.register(Rhetorica)
--- repertorium/beheer/models.py
+++ repertorium/beheer/models.py
@@ -1,14 +1,22 @@
 from django.utils import timezone
 from django.db import models
 
+class Rhetorica(models.Model):
+    jaar = models.SmallIntegerField()
+    richting = models.CharField(max_length=16)
+    titularis = models.CharField(max_length=200)
+    def __str__(self):
+        return str(self.jaar) + ' ' + self.richting + ' (' + self.titularis + ')'
+    
 class Persoon(models.Model):
     voornaam = models.CharField(max_length=200)
     achternaam = models.CharField(max_length=200)
+    rhetorica = models.ForeignKey(Rhetorica,null=True,blank=True)
     geboortedatum = models.DateField(null=True,blank=True)
     sterfdatum = models.DateField(null=True,blank=True)
     email = models.EmailField(null=True,blank=True)
     oudid = models.IntegerField('tnr_oudleerling uit oude db', null=True,blank=True)
-    wijzigdatum = models.DateField(default=timezone.now)
+    wijziging = models.DateTimeField(default=timezone.now)
     def __str__(self):
         return self.voornaam + ' ' + self.achternaam
 
@@ -18,6 +26,7 @@ class Adres(models.Model):
     van = models.DateField(null=True,blank=True)
     tot = models.DateField(null=True,blank=True)
     geldig = models.BooleanField(default=True)
-    wijzigdatum = models.DateField(default=timezone.now)
+    wijziging = models.DateTimeField(default=timezone.now)
     def __str__(self):
         return self.adres
+
