107c89757336dd91b78c5b99b71b91901301b578
Michel Vuijlsteke <michel@zog.org>
2015-07-18 09:25:38 +0200

    klas/rhetorica, contact toegevoegd, persoon uitgebreid

--- repertorium/beheer/admin.py
+++ repertorium/beheer/admin.py
@@ -1,17 +1,28 @@
 from django.contrib import admin
 
-from .models import Persoon, Rhetorica, Adres
+from .models import Persoon, Rhetorica, Adres, Contact
 
 class AdresInline(admin.TabularInline):
 	model = Adres
 	extra = 0
 
+class ContactInline(admin.TabularInline):
+	model = Contact
+	extra = 0
+
 class PersoonAdmin(admin.ModelAdmin):
 	fieldsets = [
 		(None,         {'fields': ['voornaam', 'achternaam', 'rhetorica', 'email', 'oudid']}),
-		('Personalia', {'fields': ['geslacht', 'overleden', 'geboortedatum', 'geboorteplaats', 'sterfdatum', 'sterfplaats'], 'classes': ['collapse']}),
+		('Personalia', 
+			{'fields': 
+				['geslacht', 'overleden', 
+					'geboortedatum', 'geboorteplaats', 
+					'sterfdatum', 'sterfplaats'], 
+				'classes': ['collapse']
+			}
+		),
 	]
-	inlines = [AdresInline]
+	inlines = [AdresInline,ContactInline]
 	list_display = ('voornaam', 'achternaam', 'rhetorica', 'leeftijd', 'wijziging')
 	list_filter = ['wijziging']
 	search_fields = ['voornaam', 'achternaam']
--- repertorium/beheer/models.py
+++ repertorium/beheer/models.py
@@ -2,14 +2,21 @@ import datetime
 
 from django.db import models
 from django.utils import timezone
+    
+class Klas(models.Model):
+    jaar = models.SmallIntegerField(default=datetime.datetime.now().year)
+    klasnaam = models.CharField(max_length=50,null=True,blank=True)
+    titularis = models.CharField(max_length=200,null=True,blank=True)
+    def __str__(self):
+        return str(self.jaar) + ' ' + self.klasnaam + ' (' + self.titularis + ')'
 
 class Rhetorica(models.Model):
-    jaar = models.SmallIntegerField()
+    klas = models.ForeignKey(Klas,null=True,blank=True)
+    jaar = models.SmallIntegerField(default=datetime.datetime.now().year)
     richting = models.CharField(max_length=16)
-    titularis = models.CharField(max_length=200)
     def __str__(self):
-        return str(self.jaar) + ' ' + self.richting + ' (' + self.titularis + ')'
-    
+        return str(self.jaar) + ' ' + self.richting
+
 class Persoon(models.Model):
     voornaam = models.CharField(max_length=200)
     achternaam = models.CharField(max_length=200)
@@ -22,6 +29,7 @@ class Persoon(models.Model):
     GESLACHT_ONBEKEND = '?'
     GESLACHTEN = {(GESLACHT_MAN, 'man'), (GESLACHT_VROUW, 'vrouw'), (GESLACHT_ANDER, 'ander'), (GESLACHT_ONBEKEND, 'onbekend')}
     geslacht = models.CharField(max_length=1, choices=GESLACHTEN, default=GESLACHT_MAN)    
+    
     #overige personalia
     overleden = models.BooleanField(default=False)
     geboortedatum = models.DateField(null=True,blank=True)
@@ -31,6 +39,10 @@ class Persoon(models.Model):
     
     email = models.EmailField(null=True,blank=True)
     oudid = models.IntegerField('tnr_oudleerling uit oude db', null=True,blank=True)
+    klasvertegenwoordiger = models.BooleanField(default=False)
+    publiek = models.BooleanField('mag online verschijnen?', default=True)
+    contacteren = models.BooleanField('mag gecontacteerd worden?', default=True)
+    
     wijziging = models.DateTimeField(auto_now=True)
     def leeftijd(self):
         nu = datetime.date.today()
@@ -48,3 +60,32 @@ class Adres(models.Model):
     def __str__(self):
         return self.adres
 
+class Contact(models.Model):
+    persoon = models.ForeignKey(Persoon)
+    CONTACT_TELEFOON = 'telefoon'
+    CONTACT_GSM = 'gsm'
+    CONTACT_WEBSITE = 'website'
+    CONTACT_LINKEDIN = 'linkedin'
+    CONTACT_TWITTER = 'twitter'
+    CONTACTEN = {(CONTACT_GSM, 'GSM'), (CONTACT_TELEFOON, 'Telefoon'), (CONTACT_WEBSITE, 'Website'), (CONTACT_LINKEDIN, 'LinkedIn'), (CONTACT_TWITTER, 'Twitter')}
+    contacttype = models.CharField(max_length=10, choices=CONTACTEN, default=CONTACT_GSM)
+    contactdata = models.CharField(max_length=200)
+    van = models.DateField(null=True,blank=True)
+    tot = models.DateField(null=True,blank=True)
+    geldig = models.BooleanField(default=True)
+    wijziging = models.DateTimeField(auto_now=True)
+    def __str__(self):
+        return self.contacttype + ' ' + self.contactdata
+
+class Beroep(models.Model):
+    beroepsgegevens = models.TextField()
+    van = models.DateField(null=True,blank=True)
+    tot = models.DateField(null=True,blank=True)
+    featured = models.BooleanField(default=False)
+    wijziging = models.DateTimeField(auto_now=True)
+    def __str__(self):
+        return self.beroepsgegevens
+
+class Klasfoto(models.Model):
+    klas = models.ForeignKey(Klas)
+    foto = models.ImageField(upload_to='klasfoto')
