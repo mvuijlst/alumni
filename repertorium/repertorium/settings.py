6ad60728ffee797dddc9699c2c90aa67760b4a44
Michel Vuijlsteke <michel@zog.org>
2015-07-21 02:23:43 +0200

    geen admin rechten meer voor gewoe gebruikers!

--- repertorium/repertorium/settings.py
+++ repertorium/repertorium/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'c^35y+j6(8qz4h9f0vc3ln1@w)^fvsxz%#(58!6fhe*&!-q57('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1']
 
@@ -72,6 +72,9 @@ TEMPLATES = [
 ]
         
 WSGI_APPLICATION = 'repertorium.wsgi.application'
+
+LOGIN_URL='/login'
+LOGIN_REDIRECT_URL = '/repertorium'
 
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
@@ -142,3 +145,4 @@ SUIT_CONFIG = {
     # misc
     'LIST_PER_PAGE': 15
 }
+
--- repertorium/repertorium/urls.py
+++ repertorium/repertorium/urls.py
@@ -6,7 +6,6 @@ urlpatterns = [
     url(r'^repertorium/', include('alumni.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', lambda r: HttpResponseRedirect('repertorium/')),
-    url(r'^accounts/login/$', 'django.contrib.auth.views.login',
-       {'template_name': 'admin/login.html'}),
-    url(r'^accounts/logout/$', 'django.contrib.auth.views.logout'),
+    url(r'^login$', 'django.contrib.auth.views.login'),
+    url(r'^logout$', 'django.contrib.auth.views.logout'),
 ]
