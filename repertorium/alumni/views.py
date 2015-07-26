0fc09a42fa7fa3968eeac14d26bdd22083b4c21a
Michel Vuijlsteke <michel@zog.org>
2015-07-26 13:51:33 +0200

    alleen échte alumni tonen

--- repertorium/alumni/views.py
+++ repertorium/alumni/views.py
@@ -82,7 +82,7 @@ def az(request, letter):
 		letter = "A"
 	
 	try:
-		personen = Persoon.objects.filter(achternaam__startswith=letter).order_by('achternaam', 'voornaam')
+		personen = Persoon.objects.filter(achternaam__startswith=letter).exclude(rhetorica__exact=None).order_by('achternaam', 'voornaam')
 	except Persoon.DoesNotExist:
 		raise Http404("Geen alumni gevonden.")
 	
